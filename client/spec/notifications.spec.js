1f20a1c336517b10a42b084f2e8fe684fe495e5a
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-04-24 19:35:13 +1000

    Maddi/SQ - Update carer ETA time to 20min

--- client/spec/notifications.spec.js
+++ client/spec/notifications.spec.js
@@ -3,21 +3,21 @@ const CarerNotification = require('../src/js/notifications/CarerNotification');
 
 describe('notifications', () => {
   it('all notifications should exist', () => {
-    expect(notifications).toHaveLength(3);
+    expect(notifications).toHaveLength(1);
   });
 });
 
 describe('Carer ETA notifications', () => {
   it('should have a type property with value carerETA', () => {
-    expect(notifications[2].type).toBe("carerETA");
+    expect(notifications[0].type).toBe("carerETA");
   });
   it('should have a label with Carer ETA', () => {
-    expect(notifications[2].label).toBe("Carer ETA");
+    expect(notifications[0].label).toBe("Carer ETA");
   });
-  it('should have a placeholder value with generic 5 min away message', () => {
-    expect(notifications[2].defaultValue).toBe("Your carer is 5 minutes away. Press 'right' button for more info.");
+  it('should have a placeholder value with generic 20 min away message', () => {
+    expect(notifications[0].defaultValue).toBe("Your carer is 20 minutes away. Press 'right' button for more info.");
   });
   it('should have a CarerNotification view', () => {
-    expect(notifications[2].view).toBe(CarerNotification);
+    expect(notifications[0].view).toBe(CarerNotification);
   });
 });
--- client/src/js/notifications.js
+++ client/src/js/notifications.js
@@ -1,22 +1,10 @@
-const AlertNotification = require('./notifications/AlertNotification');
 const CarerNotification = require('./notifications/CarerNotification');
 
 const notifications = [
   {
-    type: "alert",
-    label: "Alert",
-    defaultValue: "This is a test alert",
-    view: AlertNotification,
-  },
-  {
-    type: "push",
-    label: "Push",
-    defaultValue: "This is a test push notification",
-  },
-  {
     type: "carerETA",
     label: "Carer ETA",
-    defaultValue: "Your carer is 5 minutes away. Press 'right' button for more info.",
+    defaultValue: "Your carer is 20 minutes away. Press 'right' button for more info.",
     view: CarerNotification,
   },
 ];
