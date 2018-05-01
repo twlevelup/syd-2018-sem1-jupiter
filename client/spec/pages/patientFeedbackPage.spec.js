20778445bb766334334afc459c7e53674ee01757
Kevin Lai <kevin.km.lai@gmail.com>
2018-05-01 23:14:15 +1000

    Kevin: patientFeedbackPage

---
+++ client/spec/pages/patientFeedbackPage.spec.js
@@ -0,0 +1,28 @@
+const PatientFeedbackPage = require('../../src/js/pages/patientFeedbackPage');
+
+describe('The Patient Feedback Page', () => {
+
+    describe('#template', () => {
+        it('should contain "How are you feeling today?"', () => {
+            const page = new PatientFeedbackPage();
+            expect(page.template()).toContain('How are you feeling today?');
+        });
+        it('should contain a homepage icon', () => {
+            const page = new EmergencyContactPage();
+            expect(page.template()).toContain('homeIcon fas fa-home fa-2x');
+        });
+    });
+
+    describe('#topButtonEvent', () => {
+        it('goes back to homepage', () => {
+            const props = {
+                navigate: () => { },
+            };
+            const page = new PatientFeedbackPage(props);
+            spyOn(page, 'navigate');
+            // What is the previous page? (Take a reference of emergencyContactPage)
+            page.topButtonEvent();
+            expect(page.navigate).toHaveBeenCalledWith('@@@@@@@@@@@@@@@@@@@@@@@');
+        });
+    });
+});
