35bb73eaeecefeac8783f067c942f3f25e51820f
Maddi Milasas <maddi.milasas@ansarada.com>
2018-05-05 11:28:22 +1000

    Maddi/Kevin - Add long press for emergency contact

--- client/spec/pages/patientHomePage.spec.js
+++ client/spec/pages/patientHomePage.spec.js
@@ -1,24 +1,15 @@
+
 const PatientHomePage = require('../../src/js/pages/patientHomePage');
 
 describe('Patient Home Page', () => {
+    beforeEach(() => {
+        document.body.innerHTML = '<div id="button-left" />';
+    });
 
     describe('#template', () => {
         it('should contain the correct text', () => {
             const page = new PatientHomePage();
             expect(page.template()).toContain('fa-user-md');
-        });
-    });
-
-    describe('#leftButtonEvent', () => {
-        it('goes to emergency contact page', () => {
-            const props = {
-                navigate: () => { },
-            };
-            const page = new PatientHomePage(props);
-            spyOn(page, 'navigate');
-
-            page.leftButtonEvent();
-            expect(page.navigate).toHaveBeenCalledWith('emergencycontact');
         });
     });
 
@@ -32,6 +23,24 @@ describe('Patient Home Page', () => {
 
             page.rightButtonEvent();
             expect(page.navigate).toHaveBeenCalledWith('carerinfo');
+        });
+    });
+
+    describe('long press event', () => {
+        it('should get the left button', () => {
+            const page = new PatientHomePage();
+
+            expect(page.leftButton).toEqual(document.body.firstChild);
+        });
+        it('should have a hold time of 1000ms', () => {
+            const page = new PatientHomePage();
+
+            expect(page.holdTime).toEqual(1000);
+        });
+        it('should have a an initial leftButtonDown value of 0', () => {
+            const page = new PatientHomePage();
+
+            expect(page.leftButtonDown).toEqual(0);
         });
     });
 })
--- client/src/js/pages/patientHomePage.js
+++ client/src/js/pages/patientHomePage.js
@@ -4,6 +4,23 @@ const time = new Date(Date.now()).toLocaleString().split(",")[1];
 const patientHomePageTemplate = require('../../templates/patientHomePage.hbs')
 
 class PatientHomePage extends BasePage {
+    constructor(props) {
+        super(props);
+
+        this.leftButton = document.getElementById("button-left");
+        this.holdTime = 1000;
+        this.leftButtonDown = 0;
+
+        this.leftButton.addEventListener("mousedown", (e) => {
+            this.leftButtonDown = e.timeStamp;
+        });
+        this.leftButton.addEventListener("mouseup", (e) => {
+            if (e.timeStamp - this.leftButtonDown > this.holdTime) {
+                this.navigate('emergencycontact');
+            }
+        });
+    }
+
     template() {
         const context = {
             date: date,
@@ -11,10 +28,6 @@ class PatientHomePage extends BasePage {
 
         };
         return patientHomePageTemplate(context);
-    }
-
-    leftButtonEvent() {
-        this.navigate('emergencycontact');
     }
 
     rightButtonEvent() {
