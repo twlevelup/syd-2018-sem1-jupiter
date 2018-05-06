e69d4fc3765d00206c9ce00007110c2b82760b12
Maddi Milasas <maddi.milasas@ansarada.com>
2018-05-06 16:25:52 +1000

    Maddi - Require audio into CarerNotification.js

--- client/src/js/notifications/CarerNotification.js
+++ client/src/js/notifications/CarerNotification.js
@@ -3,11 +3,13 @@ const NotificationHub = require("watch-framework").NotificationHub;
 const compiledTemplate = require("../../templates/CarerNotification.hbs");
 
 const mockData = require("../../storage/my_data.json");
+const audio = require("../../sounds/definite.mp3");
 
 module.exports = class CarerNotification extends BaseNotification {
   template() {
     const context = {
       name: mockData.carers[0].name,
+      audio
     };
     return compiledTemplate(context);
   }
