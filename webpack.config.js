e62e131d5506a881112b8526f39e632f999fcc45
Maddi Milasas <maddi.milasas@ansarada.com>
2018-05-06 16:41:43 +1000

    Maddi - Place favicon.ico at root level for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
       },
       {
         test: /\.ico$/,
-        loader: "file-loader?name=[name].[ext]"
+        loader: "file-loader"
       },
       {
         test: /\.svg$/,
